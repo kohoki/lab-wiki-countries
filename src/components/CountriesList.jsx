9602e0b3bb5ca258bd0a10d2150920032f88ab5a
Ralf Bonnick <ralfbonnick@gmx.net>
2022-11-17 23:14:06 +0100

    done with it2 to 90%

--- src/components/CountriesList.jsx
+++ src/components/CountriesList.jsx
@@ -3,21 +3,19 @@ import {Link} from 'react-router-dom'
 function CountriesList({list}) {
     
   return (
-    <div>
-        {list.map((element, index) => {
-            return (
-                <div key= {index} className="col-5" style={{maxheight: '90vh'}}>
-                    <div className="list-group">
-                        <Link to={`/${element.alpha3Code}`} className="list-group-item list-group-item-action">
+    <div  className="col-5" style={{maxheight: '90vh'}}>
+        <div className="list-group">
+            {list.map((element, index) => {
+                return (
+                        <Link key= {index} to={`/${element.alpha3Code}`} className="list-group-item list-group-item-action">
                             <img
                                 src={`https://flagpedia.net/data/flags/icon/72x54/${element.alpha2Code.toLowerCase()}.png`}
                                 alt={element.name.common}/>
                             <p>{element.name.common}</p>
                         </Link>
-                    </div>
-                </div>
-            )
-        })}
+                )
+            })}
+        </div>
     </div>
   )
 }
--- src/components/CountryDetails.jsx
+++ src/components/CountryDetails.jsx
@@ -1,15 +1,56 @@
 import React from 'react'
-import { useParams, Link } from 'react-router-dom'
+import { useParams, Link} from 'react-router-dom'
+
 
 function CountryDetails({list}) {
     const { id } = useParams();
+    const newList = [...list];
     // console.log('projectId -->', id);
-    // console.log(list)
+    const foundElement = newList.filter((element)=>
+        element.alpha3Code === id
+    )
+    const borders = foundElement[0].borders
+    // console.log(borders)
   return (
     <div className="col-7">
-        <p>CountryDetails: {id}</p>
+        
+        <h1>{foundElement[0].name.common}</h1>
+            <table className="table">
+              <thead></thead>
+              <tbody>
+                <tr>
+                  <td style={{width: '30%'}}>Capital</td>
+                  <td>{foundElement[0].capital}</td>
+                </tr>
+                <tr>
+                  <td>Area</td>
+                  <td>
+                  {foundElement[0].area} km
+                    <sup>2</sup>
+                  </td>
+                </tr>
+                <tr>
+                  <td>Borders</td>
+                  <td>
+                    <ul>
+                        {borders.map((element, index)=>{
+                            {newList.map((element2)=>{     
+                                {if(element === element2.alpha3Code){
+                                    {/* {console.log(element2.name.common)} */}
+                                    return (       
+                                        <Link to={`/${element2.alpha3Code}`}>
+                                            <li>{element2.name.common}</li>
+                                        </Link>
+                                        ) 
+                                }}
+                            })}
+                        })}
+                    </ul>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
     </div>
-    
   )
 }
 
